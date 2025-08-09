4780eb078c6a2d3350abe2eb2be842e4f904b8c1
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-09 11:06:15 -0500

    update

--- middleware.ts
+++ middleware.ts
@@ -21,7 +21,7 @@ export function middleware(request: NextRequest) {
     wss://ws-us3.pusher.com
     https://api.iconify.design/
     ;
-    script-src 'self' 'nonce-${nonce}' https://va.vercel-scripts.com https://vercel.live/;
+    script-src 'self' 'nonce-${nonce}' https://vercel.live/;
     style-src 'self' 'unsafe-inline' https://vercel.live;
     img-src 'self' https://vercel.live https://vercel.com blob: data:;
     font-src 'self' https://vercel.live https://assets.vercel.com;
