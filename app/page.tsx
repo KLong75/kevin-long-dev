c08e6643cfd33be328d9582dd1a3ce10fad116fd
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-10 15:03:20 -0500

    update container

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   await connection();
   return (
     <div className="flex flex-1 items-center justify-center w-full">
-      <div className="relative w-[80vw] 3xl:w-[60vw] h-[80vh] 3xl:h-[60vh] flex items-center justify-center">
+      <div className="relative w-[80vw] h-[80vh] flex items-center justify-center">
         <PopcornIcons />
         <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
           <h2 className="text-green-500 text-xl md:text-2xl lg:text-4xl font-bold text-center">
