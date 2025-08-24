384afe25443cc1e70bbf2dfcb1764d2bbe1e104b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-24 10:07:45 -0500

    tech badge styles

--- ui/tech-badge.tsx
+++ ui/tech-badge.tsx
@@ -6,7 +6,7 @@ export default function TechBadge({
   documentationUrl: string;
 }) {
   return (
-    <li className="font-share-tech-mono text-shadow-white-thin font-bold m-2 border-4 border-zinc-900 shadow-md shadow-zinc-200/50 text-center rounded-2xl text-black text-sm bg-green-500 p-1">
+    <li className="font-share-tech-mono text-shadow-white-thin font-bold m-2 border-4 border-zinc-900 shadow-md shadow-green-500/50 text-center rounded-2xl text-black text-sm bg-green-500 p-1">
       <a
         href={documentationUrl}
         target="_blank"
