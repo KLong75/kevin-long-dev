026c6d318d18ad72c2dff32601b3bdc262fab66d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-26 21:46:29 -0500

    update

--- ui/github-contribution-grid.tsx
+++ ui/github-contribution-grid.tsx
@@ -2,7 +2,7 @@
 // import github calendar
 import GitHubCalendar from "react-github-calendar";
 // import from mui
-import { Tooltip as MuiTooltip } from '@mui/material'
+import { Tooltip as MuiTooltip } from "@mui/material";
 // import components
 import ContactIconLinks from "./contact-icon-links";
 
@@ -30,28 +30,47 @@ export default function GitHubContributionGrid() {
 
   return (
     <div className="w-full max-w-10xl flex flex-col items-center my-16 contribution-shadow custom-scrollbar md:shadow-2xl md:shadow-green-500/50 p-2 md:p-12 rounded-2xl">
-        <h3 className="text-2xl 3xl:text-3xl font-bold text-center -mt-4 mb-6">
-          GitHub Contributions
-        </h3>
+      <h3 className="text-2xl 3xl:text-3xl font-bold text-center -mt-4 mb-6">
+        GitHub Contributions
+      </h3>
       <GitHubCalendar
         username="KLong75"
-        blockSize={15}
-        blockMargin={5}
+        blockSize={14}
+        blockMargin={6}
         colorScheme="dark"
         fontSize={14}
         renderBlock={(block, activity) => (
-          <MuiTooltip title={`${activity.count} activities on ${activity.date}`}>
+          <MuiTooltip
+            title={`${activity.count} ${
+              activity.count === 1 ? "contribution" : "contributions"
+            } on ${new Date(activity.date).toLocaleDateString("en-US", {
+              month: "long",
+              day: "2-digit",
+              year: "numeric",
+            })}`}
+            // slotProps={{
+            //   tooltip: {
+            //     sx: {
+            //       color: "#000",
+            //       backgroundColor: "#e0e0e0", 
+            //     },
+            //   },
+            // }}
+            >
             {block}
           </MuiTooltip>
         )}
       />
       <span className="text-sm italic mt-2">
-          {oneYearAgoPlusOneDayFormatted} - {todayFormatted}
+        {oneYearAgoPlusOneDayFormatted} - {todayFormatted}
       </span>
       <div className="mt-4 -mb-4">
-        <ContactIconLinks orientation="horizontal" include={["GitHub"]} size={36}/>
+        <ContactIconLinks
+          orientation="horizontal"
+          include={["GitHub"]}
+          size={36}
+        />
       </div>
     </div>
   );
 }
-
