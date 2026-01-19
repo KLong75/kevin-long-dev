30d569618acb5e068fb5a33722a011b2006bfb96
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-01-19 09:32:58 -0600

    fix GH date

--- ui/github-contribution-grid.tsx
+++ ui/github-contribution-grid.tsx
@@ -11,22 +11,21 @@ import ContactIconLinks from "./contact-icon-links";
 export default function GitHubContributionGrid() {
   const [dateRange, setDateRange] = useState({ start: "", end: "" });
   useEffect(() => {
-    const today = new Date().toISOString();
-    const todayFormatted = new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    }).format(new Date());
-    const oneYearAgo = new Date();
-    const oneYearAgoPlusOneDay = new Date(oneYearAgo);
-    oneYearAgoPlusOneDay.setDate(oneYearAgoPlusOneDay.getDate() - 1);
-    const oneYearAgoPlusOneDayFormatted = new Intl.DateTimeFormat("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    }).format(oneYearAgoPlusOneDay);
-    setDateRange({ start: oneYearAgoPlusOneDayFormatted, end: todayFormatted });
-  }, []);
+  const today = new Date();
+  const todayFormatted = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(today);
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+  const oneYearAgoFormatted = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(oneYearAgo);
+  setDateRange({ start: oneYearAgoFormatted, end: todayFormatted });
+}, []);
 
   return (
     <div className="p-4 mx-2">
