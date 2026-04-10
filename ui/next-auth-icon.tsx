5bb820ea841d0d222b203c1f2cb27d2d77662734
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-04-10 16:33:19 -0500

    add caret color to input form, fix warning on next auth icon img

--- ui/next-auth-icon.tsx
+++ ui/next-auth-icon.tsx
@@ -2,17 +2,14 @@ import Image from "next/image";
 
 export function NextAuthIcon() {
   return (
-    <div>
-      {" "}
-      <Image
-        src="/tech-icons/next-auth.png" // Place your PNG at public/icons/next-auth.png
-        alt="NextAuth.js"
-        width={32}
-        height={32}
-        style={{ display: "inline-block" }}
-        draggable={false}
-        priority
-      />
-    </div>
+    <Image
+      src="/tech-icons/next-auth.png"
+      alt="NextAuth.js"
+      fill
+      draggable={false}
+      priority
+      className="object-contain"
+      sizes="42px"
+    />
   );
 }
